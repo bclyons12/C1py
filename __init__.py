ffc3beda974891aff5da1a88f95fc29f7434c609
Brendan Lyons <lyonsbc@fusion.gat.com>
2020-04-09 15:51:42 -0400

    Remove cruft from __init__.py

--- __init__.py
+++ __init__.py
@@ -8,70 +8,20 @@ Date edited:
 """
 from importlib import reload
 
-#import C1py.load_Bmns
-#reload(C1py.load_Bmns)
 from C1py.load_Bmns import load_Bmns
-
-#import C1py.load_Bwall
-#reload(C1py.load_Bwall)
 from C1py.load_Bwall import load_Bwall
-
-#import C1py.load_Bres
-#reload(C1py.load_Bres)
 from C1py.load_Bres import load_Bres
-
-#import C1py.multi_Bres
-#reload(C1py.multi_Bres)
 from C1py.multi_Bres import multi_Bres
-
-#import C1py.plot_Bmns
-#reload(C1py.plot_Bmns)
 from C1py.plot_Bmns import plot_Bmns, plot_Bmn
-
-#import C1py.plot_Bwall
-#reload(C1py.plot_Bwall)
 from C1py.plot_Bwall import plot_Bwall
-
-#import C1py.calc_puc
-#reload(C1py.calc_puc)
 from C1py.calc_puc import calc_puc
-
-#import C1py.calc_puc_multi
-#reload(C1py.calc_puc_multi)
 from C1py.calc_puc_multi import calc_puc_multi
-
-#import C1py.calc_puc_3
-#reload(C1py.calc_puc_3)
 from C1py.calc_puc_3 import calc_puc_3
-
-#import C1py.calc_puc_3_multi
-#reload(C1py.calc_puc_3_multi)
 from C1py.calc_puc_3_multi import calc_puc_3_multi
-
-#import C1py.plot_omegas
-#reload(C1py.plot_omegas)
 from C1py.plot_omegas import plot_omegas
-
-#import C1py.calc_ExB
-#reload(C1py.calc_ExB)
 from C1py.calc_ExB import calc_ExB
-
-#import C1py.read_field
-#reload(C1py.read_field)
 from C1py.read_field import read_field
-
-#import C1py.plot_field
-#reload(C1py.plot_field)
 from C1py.plot_field import plot_field
-
-#import C1py.plot_shape
-#reload(C1py.plot_shape)
 from C1py.plot_shape import plot_shape
-
-#import C1py.plot_contour
-#reload(C1py.plot_contour)
 from C1py.plot_contour import plot_contour
-
-#import C1py.vary_sfp
-#reload(C1py.vary_sfp)
 from C1py.vary_sfp import vary_sfp
